22fbc721b978fc37c233c3d42fe04a0e4816c9ec
Mohamed AbdElZaher <hamada6_4@hotmail.com>
2021-07-18 12:14:14 -0500

    Add some fail test cases

--- lesson5.js
+++ lesson5.js
@@ -82,15 +82,59 @@ function myFunctionTest(expected, function2test) {
         }
     }
 }
+// Expected SUCCEEDED
 console.log(myFunctionTest(33, function () { return max(19, 33); }));
+// Expected FAILED
+console.log(myFunctionTest(3, function () { return max(8, 3); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest(49, function () { return maxOfThree(8, 49, 18); }));
+// Expected FAILED
+console.log(myFunctionTest(18, function () { return maxOfThree(8, 49, 18); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest(true, function () { return isVowel("i"); }));
+// Expected FAILED
+console.log(myFunctionTest(false, function () { return isVowel("a"); }));
+// Expected SUCCEEDED
 console.log(myFunctionTest(false, function () { return isVowel("m"); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest(31, function () { return sum([5, 9, 8, 6, 3]); }));
+// Expected FAILED
+console.log(myFunctionTest(30, function () { return sum([5, 9, 8, 6, 3]); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest(192, function () { return multiply([3, 2, 4, 8]); }));
+// Expected FAILED
+console.log(myFunctionTest(105, function () { return multiply([3, 2, 4, 8]); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest("demahoM", function () { return reverse("Mohamed"); }));
+// Expected FAILED
+console.log(myFunctionTest("Mohamed", function () { return reverse("Mohamed"); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest(7, function () { return findLongestWord(["cat", "Ahmed", "Mohamed", "look", "Hello", "I"]); }));
+// Expected FAILED
+console.log(myFunctionTest(6, function () { return findLongestWord(["cat", "Ahmed", "Mohamed", "look", "Hello", "I"]); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest(["Ahmed", "Mohamed", "Hello"], function () { return filterLongWords(["cat", "Ahmed", "Mohamed", "look", "Hello", "I"], 4); }));
+// Expected FAILED
+console.log(myFunctionTest(["cat", "Ahmed", "Mohamed", "Hello"], function () { return filterLongWords(["cat", "Ahmed", "Mohamed", "look", "Hello", "I"], 4); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest([30, 90, 100, 150, 20, 700], function () { return multiplyby10([3, 9, 10, 15, 2, 70]); }));
+// Expected FAILED
+console.log(myFunctionTest([30, 90, 100, 1500, 20, 700], function () { return multiplyby10([3, 9, 10, 15, 2, 70]); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest([3, 3, 3], function () { return getEqualTo([3, 9, 3, 15, 3, 70], 3); }));
+// Expected FAILED
+console.log(myFunctionTest([3, 3, 5], function () { return getEqualTo([3, 9, 3, 15, 3, 70], 3); }));
+
+// Expected SUCCEEDED
 console.log(myFunctionTest(378, function () { return getProduct([2, 3, 9, 7]); }));
+// Expected FAILED
+console.log(myFunctionTest(350, function () { return getProduct([2, 3, 9, 7]); }));
